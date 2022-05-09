7786ebaed1892e6734b3d21ca9d73e322ef10fdc
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-10 00:11:41 +0300

    This commit solves bugs on css

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,7 @@ def create_app(config_name):
 
     app.register_blueprint(authentication_blueprint)
     app.register_blueprint(main_blueprint)
-
+    app.static_folder = 'static'
     login_manager.init_app(app)
     db.init_app(app)
     bootstap.init_app(app)
