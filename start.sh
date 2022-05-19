1d9857d226036a09f6bb5232c2c2e48b0ecbb39c
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-19 10:26:45 +0300

    This commit updates on deployment

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-# export SECRET_KEY=os.environ.get('SECRET_KEY')
-# export SQLALCHEMY_DATABASE_URI = 'postgresql://iiljmuorilputx:f604b9247d0b76c3d418eafdc3cc692961cf41572bf2b69c1b66a820b077774e@ec2-3-217-113-25.compute-1.amazonaws.com:5432/dep5de82na2p6d'
+export SECRET_KEY='SECRET_KEY'
+export SQLALCHEMY_DATABASE_URI = 'postgresql://iiljmuorilputx:f604b9247d0b76c3d418eafdc3cc692961cf41572bf2b69c1b66a820b077774e@ec2-3-217-113-25.compute-1.amazonaws.com:5432/dep5de82na2p6d'
 
-#  python3.8 manage.py server
+ python3.8 manage.py server
